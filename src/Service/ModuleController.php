dcdab6d1e58db8423f5db16b7809120ffa831478
ZoDream <648383079@qq.com>
2017-08-18 23:42:16 +0800

    修改安装模块

--- src/Service/ModuleController.php
+++ src/Service/ModuleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Zodream\Module\Gzo\Service;
 
+use Zodream\Helpers\Str;
 use Zodream\Service\Factory;
 
 class ModuleController extends Controller {
@@ -10,11 +11,53 @@ class ModuleController extends Controller {
     }
 
     public function installAction($name, $module) {
-        $configs = Factory::config()->getCurrentFile();
+        $configs = $this->getConfigs();
+        $configs['modules'][$name] = $module;
+        $this->invokeModuleMethod($module, 'install');
+        $this->saveConfigs($configs);
+        return $this->showContent('true');
     }
 
     public function uninstallAction($name) {
+        $configs = $this->getConfigs();
+        if (isset($configs['modules'][$name])) {
+            $this->invokeModuleMethod($configs['modules'][$name], 'uninstall');
+            unset($configs['modules'][$name]);
+            $this->saveConfigs($configs);
+        }
+        return $this->showContent('true');
+    }
 
+    protected function getModule($module) {
+        if (Str::endWith($module, 'Module')) {
+            return $module;
+        }
+        return $module.'\\Module';
+    }
+
+    protected function invokeModuleMethod($module, $methods) {
+        $module = $this->getModule($module);
+        if (!class_exists($module)) {
+            return;
+        }
+        $instance = new $module;
+        foreach ((array)$methods as $method) {
+            if (empty($method) || !method_exists($instance, $method)) {
+                continue;
+            }
+            call_user_func([$instance, $method]);
+        }
+    }
+
+    protected function getConfigs() {
+        return include Factory::config()->getCurrentFile()->getFullName();
+    }
+
+    protected function saveConfigs($configs) {
+        Factory::config()->getCurrentFile()->write(Factory::view()
+            ->render('Template/config', array(
+                'data' => $configs
+            )));
     }
 
 }
--- src/UserInterface/Template/config.php
+++ src/UserInterface/Template/config.php
@@ -11,22 +11,22 @@ echo '<?php';
 
 return [
 <?php foreach ($data as $key => $item):?>
-    <?php if (!is_array($item)):?>
+<?php if (!is_array($item)):?>
     '<?=$key?>' => '<?=$item?>',
-    <?php else:?>
+<?php else:?>
     '<?=$key?>' => [
-        <?php foreach ($item as $k => $it):?>
-        <?php if (!is_array($it)):?>
-            '<?=$k?>' => '<?=$it?>',
-        <?php else:?>
-            '<?=$k?>' => [
-            <?php foreach ($it as $k1 => $it1):?>
+<?php foreach ($item as $k => $it):?>
+<?php if (!is_array($it)):?>
+        '<?=$k?>' => '<?=$it?>',
+<?php else:?>
+        '<?=$k?>' => [
+<?php foreach ($it as $k1 => $it1):?>
                 '<?=$k1?>' => '<?=$it1?>',
-            <?php endforeach;?>
-            ],
-        <?php endif;?>
-        <?php endforeach;?>
+<?php endforeach;?>
+        ],
+<?php endif;?>
+<?php endforeach;?>
     ],
-    <?php endif;?>
+<?php endif;?>
 <?php endforeach;?>
 ];
