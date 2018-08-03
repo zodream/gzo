9fb375e4fe582553f86a8a51dece62b63e01e2bf
zodream <648383079@qq.com>
2018-08-03 12:40:21 +0800

    修改

--- src/Service/HomeController.php
+++ src/Service/HomeController.php
@@ -6,7 +6,6 @@ use Zodream\Html\Bootstrap\Html;
 use Zodream\Module\Gzo\Domain\Database\Schema;
 use Zodream\Module\Gzo\Domain\GenerateModel;
 use Zodream\Service\Factory;
-use Zodream\Infrastructure\Http\URL;
 
 class HomeController extends Controller {
 
@@ -61,24 +60,24 @@ class HomeController extends Controller {
     public function sqlAction($query = null, $schema = null, $table = null, $action = null) {
         if ($action == 'optimize' && !empty($table)) {
             $crumbs = [
-                '服务器：localhost' => Url::to('./home/sql'),
-                '数据库：'.$schema => Url::to('./home/sql', compact('schema')),
-                '表：'.$table => Url::to('./home/sql', compact('schema', 'table')),
+                '服务器：localhost' => url('./home/sql'),
+                '数据库：'.$schema => url('./home/sql', compact('schema')),
+                '表：'.$table => url('./home/sql', compact('schema', 'table')),
                 '优化'
             ];
             GenerateModel::schema($schema)->table($table)->optimize();
             $data = [['提示' => '优化成功！']];
         } elseif (!empty($query)) {
             $crumbs = [
-                '服务器：localhost' => Url::to('./home/sql'),
-                '数据库：'.$schema => Url::to('./home/sql', compact('schema')),
+                '服务器：localhost' => url('./home/sql'),
+                '数据库：'.$schema => url('./home/sql', compact('schema')),
                 '执行Sql结果'
             ];
             $data = GenerateModel::schema($schema)->getRows($query);
         } elseif (!empty($table)) {
             $crumbs = [
-                '服务器：localhost' => Url::to('./home/sql'),
-                '数据库：'.$schema => Url::to('./home/sql', compact('schema')),
+                '服务器：localhost' => url('./home/sql'),
+                '数据库：'.$schema => url('./home/sql', compact('schema')),
                 '表：'.$table
             ];
             $data = GenerateModel::schema($schema)->table($table)->getAllColumn(true);
@@ -91,30 +90,30 @@ class HomeController extends Controller {
             }, $data);
         } elseif (!empty($schema)) {
             $crumbs = [
-                '服务器：localhost' => Url::to('./home/sql'),
+                '服务器：localhost' => url('./home/sql'),
                 '数据库：'.$schema
             ];
             $data = GenerateModel::schema($schema)->getAllTable(true);
             $data = array_map(function ($item) {
                 $tip = '';
                 if ($item['Data_free'] > 0) {
-                    $tip = sprintf('(可%s)', Html::a('优化', Url::to(null, ['table' => $item['Name'], 'action' => 'optimize'])));
+                    $tip = sprintf('(可%s)', Html::a('优化', url(null, ['table' => $item['Name'], 'action' => 'optimize'])));
                 }
                 $args = [
                     '数据表' =>
-                    Html::a($item['Name'], Url::to(null, ['table' => $item['Name']])).$tip
+                    Html::a($item['Name'], url(null, ['table' => $item['Name']])).$tip
                 ];
                 unset($item['Name']);
                 return $args + $item;
             }, $data);
         } else {
             $crumbs = [
-                '服务器：localhost' => Url::to('./home/sql')
+                '服务器：localhost' => url('./home/sql')
             ];
             $data = Schema::getAllDatabase();
             $data = array_map(function ($item) {
                 return [
-                    '数据库' => Html::a($item['Database'], Url::to(null, ['schema' => $item['Database']]))
+                    '数据库' => Html::a($item['Database'], url(null, ['schema' => $item['Database']]))
                 ];
             }, $data);
         }
