8291a102b1d03b2b13a17389a3f974fb67aa258f
zodream <648383079@qq.com>
2018-10-09 14:00:37 +0800

    修改

---
+++ src/Service/ComposerController.php
@@ -0,0 +1,62 @@
+<?php
+namespace Zodream\Module\Gzo\Service;
+
+use Zodream\Helpers\Str;
+use Zodream\Module\Gzo\Domain\Generator\ModuleGenerator;
+use Zodream\Service\Factory;
+
+class ComposerController extends Controller {
+
+
+    public function initAction() {
+        $file = Factory::root()->file('Service/config/config.php');
+        if ($file->exist()) {
+            return;
+        }
+
+        $content = ModuleGenerator::renderTemplate('config', [
+            'data' => $this->generateConfigs()
+        ]);
+        $file->write($content);
+        return;
+    }
+
+    protected function generateRandomKey() {
+        return 'base64:'.base64_encode(
+                Str::randomBytes(32)
+            );
+    }
+
+    protected function generateConfigs() {
+        $key = $this->generateRandomKey();
+        return [
+            'app' => [
+                'host' => 'localhost',
+                'key' => $key
+            ],
+            'db' => [
+                'database' => 'zodream',
+                'user' => 'root',
+                'password' => 'root',
+                'prefix' => '',
+            ],
+            'auth' => [
+                'home' => '/auth',
+                'model' => 'Module\Auth\Domain\Model\UserModel',
+            ],
+            'mail' => [
+                'host' => null,
+                'port' => '25',
+                'user' => null,
+                'name' => 'ZoDream',
+                'password' => null,
+            ],
+            'modules' => [
+
+            ],
+            'routes' => [
+
+            ],
+        ];
+    }
+}
