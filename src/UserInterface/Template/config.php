106ca1f0281c140c280c3709f54ef059b7ffcdf9
ZoDream <648383079@qq.com>
2019-04-17 23:22:52 +0800

    修改

--- src/UserInterface/Template/config.php
+++ src/UserInterface/Template/config.php
@@ -21,7 +21,7 @@ return [
 <?php else:?>
         '<?=$k?>' => [
 <?php foreach ($it as $k1 => $it1):?>
-                '<?=$k1?>' => '<?=is_array($it1) ? var_export($it1, true) : $it1 ?>',
+                '<?=$k1?>' => <?=var_export($it1, true) ?>,
 <?php endforeach;?>
         ],
 <?php endif;?>
