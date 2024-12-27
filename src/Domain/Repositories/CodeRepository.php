60b11c94da9a21711eec1191f7c27093c3511630
ZoDream <648383079@qq.com>
2024-12-27 17:10:15 +0800

    update

--- src/Domain/Repositories/CodeRepository.php
+++ src/Domain/Repositories/CodeRepository.php
@@ -62,10 +62,9 @@ class CodeRepository {
 
         $content = str_replace(
             ['$this.', '\'', '$', '""', 'RoleRepository.NewPermission', 'RoleRepository.NewRole',
-                'Option.Group', 'Model.CREATED_AT', '"created_at"', '"updated_at"'],
+                'Option.Group', 'Model.CREATED_AT', 'Model.UPDATED_AT'],
             ['', '"', '', 'string.Empty', 'privilege.AddPermission', 'privilege.AddRole',
-                'option.AddGroup', 'MigrationTable.COLUMN_CREATED_AT',
-                'MigrationTable.COLUMN_CREATED_AT', 'MigrationTable.COLUMN_UPDATED_AT'],
+                'option.AddGroup', '"created_at"', '"updated_at"'],
             $content);
         $content = preg_replace_callback('/class\s+(\w+)([^\{]*)/', function ($match) use($content) {
             $name = static::studly($match[1]);
@@ -106,6 +105,52 @@ class CodeRepository {
         $content = preg_replace_callback('/const\s+([^;]+);/', function ($match) {
             return sprintf('public const %s %s;', str_contains($match[1], '"') ? 'string' : 'int', $match[1]);
         }, $content);
+        if (str_contains($content, 'Migration(db)')) 
+        {
+            $content = preg_replace_callback('/table(\.[\s\S]+?);/', function ($match) {
+                $uniqueItems = [];
+                $lastKey = '';
+                $line = preg_replace_callback('/\.(\w+)\(([^\)]*)\)/', function ($m) use (&$uniqueItems, &$lastKey) {
+                    if ($m[1] === 'Id') 
+                    {
+                        return ".HasKey(table => table.Id);\n            builder.Property(table => table.Id).HasColumnName(\"id\")";
+                    }
+                    if ($m[1] === 'Timestamps') {
+                        return ".Property(table => table.UpdatedAt).HasColumnName(\"updated_at\");\n            builder.Property(table => table.CreatedAt).HasColumnName(\"created_at\")";
+                    }
+                    if ($m[1] === 'SoftDeletes') {
+                        return ".Property(table => table.DeletedAt).HasColumnName(\"deleted_at\")";
+                    }
+                    if ($m[1] === 'Column') {
+                        $lastKey = static::studly(trim($m[2], '"'));
+                        return sprintf('.Property(table => table.%s).HasColumnName(%s)', $lastKey, $m[2]);
+                    }
+                    if (in_array($m[1], ['String', 'Int', 'Uint', 'Text', 'Varchar', 'Char', 'Timestamp', 'Bool', 'Date', 'Decimal', 'Enum', 'Float', 'Time'])) {
+                        $temp = explode(',', $m[2]);
+                        $lastKey = static::studly(trim($temp[0], '"'));
+                        $res = sprintf('.Property(table => table.%s).HasColumnName(%s)', $lastKey, $temp[0]);
+                        if (count($temp) > 1) {
+                            $res .= sprintf('.HasMaxLength(%d)', $temp[1]);
+                        }
+                        return $res;
+                    }
+                    if ($m[1] === 'Default') {
+                        return sprintf('.HasDefaultValue(%s)', $m[2]);
+                    }
+                    if ($m[1] === 'Comment') {
+                        return sprintf('.HasComment(%s)', $m[2]);
+                    }
+                    if ($m[1] === 'Unique') {
+                        $uniqueItems[] = $lastKey;
+                    }
+                    return '';
+                }, $match[1]);
+                foreach ($uniqueItems as $key) {
+                    $line = sprintf(".HasIndex(table => table.%s).IsUnique();\n            builder%s", $key, $line);
+                }
+                return sprintf('builder%s;', $line);
+            }, $content);
+        }
         return $content;
     }
 
